fa97c0199d72afc97a50a86296755c47795c35fe
Piyush Kumar Yadav <73697893+piyush1212@users.noreply.github.com>
2024-04-05 12:47:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  });
+}
+
+// Initially expand the first panel
+panels[0].classList.add('active');
